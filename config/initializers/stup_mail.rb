1f6b416cef23bc90d9edad4886f22ab83c2dd65d
unknown <sachin.singh@rdias.ac.in>
2013-07-10 14:03:39 +0530

    change in mail password and  contact file

--- config/initializers/stup_mail.rb
+++ config/initializers/stup_mail.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :domain => 'railscast.com',
   :user_name => 'sachinsingh.7903@gmail.com',
-  :password => 'sachin22196095',
+  :password => 'singh22196095',
   :enable_starttls_auto => true
 }
 
